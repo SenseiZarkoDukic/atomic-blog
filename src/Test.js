5e7eee9223eba2470faeae5cc7cbbac5b1c6eb88
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-02 16:15:08 +0100

    Put SlowComponent as a children Prop so it will not rerender when inside and the parent component is rendered

--- src/Test.js
+++ src/Test.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Children, useState } from "react";
 
 function SlowComponent() {
   // If this is too slow on your maching, reduce the `length`
@@ -14,13 +14,28 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
     </div>
+  );
+}
+
+export default function Test() {
+  // const [count, setCount] = useState(0);
+  return (
+    //   <div>
+    //     <h1>Slow counter?!?</h1>
+    //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+    //     <SlowComponent />
+    // </div>
+
+    <Counter>
+      <SlowComponent />
+    </Counter>
   );
 }
