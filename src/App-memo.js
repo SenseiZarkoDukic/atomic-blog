9e855321f78a91e66982f155ecca91fd0bf6f7a3
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-03 15:03:05 +0100

    Test setter function if it will rerender archive post if no memoized

--- src/App-memo.js
+++ src/App-memo.js
@@ -64,7 +64,11 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive archiveOptions={archiveOptions} onAddPost={handleAddPost} />
+      <Archive
+        archiveOptions={archiveOptions}
+        onAddPost={handleAddPost}
+        setIsFakeDark={setIsFakeDark}
+      />
       <Footer />
     </section>
   );
@@ -161,7 +165,11 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ archiveOptions, onAddPost }) {
+const Archive = memo(function Archive({
+  archiveOptions,
+  onAddPost,
+  setIsFakeDark,
+}) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
