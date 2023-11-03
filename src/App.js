11d341446b835b7d51c99b3dc05651ee5c206620
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-11-03 16:01:49 +0100

    Memo object of value in PostContext.Provider and function of Main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { PostProvider, usePosts } from "./PostContext";
-import Test from "./Test";
 
 function createRandomPost() {
   return {
@@ -73,14 +72,14 @@ function Results() {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-function Main() {
+const Main = memo(function Main() {
   return (
     <main>
       <FormAddPost />
       <Posts />
     </main>
   );
-}
+});
 
 function Posts() {
   return (
